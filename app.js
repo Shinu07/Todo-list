c9237da8b345c511e26e612f6a544b188fde6be8
Soumya ranjan Purohit <91987369+Shinu07@users.noreply.github.com>
2024-02-02 01:20:38 +0530

    #initial

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-const inputBox = document.getElementById("task-input");
-const listContainer = document.getElementById("list-container");
-function addTask(){
-    if (inputBox.value === ''){
-        alert('write your task')
-    }else {
-        let li = document.createElement("li")
-        li.innerHTML = inputBox.value
+const inputBox = document.querySelector("#task-input");
+const listContainer = document.querySelector("#list-container");
+const addBtn = document.querySelector(".add-btn");
+
+//when its empty it give alert message else it execute task on list
+
+const addTask=()=>{
+    if(inputBox.value === ''){
+        alert(`Enter your Todo Tasks`)
+    }else{
+        let li = document.createElement('li');
+        li.innerText = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement('span');
-        span.innerHTML = '\u00d7';
+        span.innerText = '\u00d7';
         li.appendChild(span);
     }
-    inputBox.value = "";
-    saveData();
+
+    inputBox.value = '';
 }
+
+//if i click on li element it toggle(checked/uncheked) and if i click on span it remove the whole list
 listContainer.addEventListener('click', (e)=>{
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle('checked');
-        saveData()
-    }else if(e.target.tagName === "SPAN"){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked')
+    }else if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove()
-        saveData();
     }
-},false);
+
+},false)
 
 function saveData(params) {
     localStorage.setItem('data',listContainer.innerHTML)
+}
+
+inputBox.addEventListener("keydown", handleKey);
+addBtn.addEventListener("click", handleKey);
+
+function handleKey(e) {
+    if (e.type === "click" || e.key === "Enter") {
+        // e.preventDefault(); // Prevents the default behavior of the Enter key (form submission)
+        addTask();
+    }
 }
