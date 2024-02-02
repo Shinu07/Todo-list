39b519d0516e4d528368fd31a90c0fe6547f6106
Soumya ranjan Purohit <91987369+Shinu07@users.noreply.github.com>
2024-02-03 01:28:19 +0530

    #improve functionlity

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const addBtn = document.querySelector(".add-btn");
 //when its empty it give alert message else it execute task on list
 
 const addTask = () => {
-  if (inputBox.value === "") {
+  const textInput = inputBox.value.trim();
+  if (!textInput) {
     alert(`Enter your Todo Tasks`);
   } else {
     let li = document.createElement("li");
@@ -17,24 +18,37 @@ const addTask = () => {
   }
 
   inputBox.value = "";
+  saveData();
 };
 
-//if i click on li element it toggle(checked/uncheked) and if i click on span it remove the whole list
+//if i click on li element it toggle(checked/uncheked) and if i click on span element it remove the whole list
 listContainer.addEventListener(
   "click",
   (e) => {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
+      saveData();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
+      saveData();
     }
   },
   false
 );
 
-function saveData(params) {
+// it save listcontainer data in local Storage
+function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
+
+//after reload  it show saved data in local storage, if it exist
+function showTask() {
+  const savedData = localStorage.getItem("data");
+  if (savedData) {
+    listContainer.innerHTML = savedData;
+  }
+}
+showTask();
 
 inputBox.addEventListener("keydown", handleKey);
 addBtn.addEventListener("click", handleKey);
